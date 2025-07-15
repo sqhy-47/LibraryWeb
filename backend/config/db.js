3685f289373a07f2dcc99feac1fc12f296abcf68
Angela Gao <ag8969@nyu.edu>
2025-07-15 16:27:45 -0400

    Fixed a small type from mongo_url to uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const connectdb = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error){
         console.error(`Error: ${error.message}`);
